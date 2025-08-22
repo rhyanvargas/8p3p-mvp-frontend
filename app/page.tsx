81330fe705977007a4ef4feeb326cef14400312c
Rhyguy <rhyan@rhyanvargas.com>
2025-08-21 21:36:37 -0700

    feat: add delete functionality to todo items with updated button UI

--- app/page.tsx
+++ app/page.tsx
@@ -13,40 +13,47 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+	const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
+	function listTodos() {
+		client.models.Todo.observeQuery().subscribe({
+			next: (data) => setTodos([...data.items]),
+		});
+	}
 
-  useEffect(() => {
-    listTodos();
-  }, []);
+	function deleteTodo(id: string) {
+		client.models.Todo.delete({ id });
+	}
 
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+	useEffect(() => {
+		listTodos();
+	}, []);
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+	function createTodo() {
+		client.models.Todo.create({
+			content: window.prompt("Todo content"),
+		});
+	}
+
+	return (
+		<main>
+			<h1>My todos</h1>
+			<button onClick={createTodo}>+ new</button>
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo.id}>
+						{todo.content}
+						<button onClick={() => deleteTodo(todo.id)}>Delete</button>
+					</li>
+				))}
+			</ul>
+			<div>
+				🥳 App successfully hosted. Try creating a new todo.
+				<br />
+				<a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+					Review next steps of this tutorial.
+				</a>
+			</div>
+		</main>
+	);
 }
